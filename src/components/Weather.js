35c4f03082ee14360d75a8503fb49a45acb2f5c2
karlo <deguzmankarlo@gmail.com>
2020-05-25 22:58:42 +0100

    Spike NASA API call

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,50 @@
 import React, {Component} from 'react';
 
 class Weather extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      weatherData: {}
+    };
+  }
+
+  componentDidMount() {
+    const nasaAPI = 'https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0'
+    fetch(nasaAPI)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            weatherData: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
   render() {
-    return(
-      <div></div>
-    )
+    const { error, isLoaded, weatherData } = this.state;
+    const solKeys = weatherData.sol_keys
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          {solKeys.map(sol => <div key={sol}>Sol {sol} : {weatherData[sol].AT.av}°C</div>)}
+        </div>
+      );
+    }
   }
 }
 
