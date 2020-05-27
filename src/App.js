bda100408d4866519211d3ff0995d354787fcabd
karlo <deguzmankarlo@gmail.com>
2020-05-27 22:52:25 +0100

    Add title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Weather from './components/Weather';
 function App() {
   return (
     <div className="hero is-fullheight has-background">
+      <div className="title is-size-1">Mars Weather</div>
       <img className="hero-background is-transparent" src="https://www.nasa.gov/sites/default/files/thumbnails/image/pia22228.jpg" alt=""></img>
       <Weather />
     </div>
