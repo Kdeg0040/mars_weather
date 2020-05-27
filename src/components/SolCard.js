1d7ee5352d2725295154fd4012470411399f53db
karlo <deguzmankarlo@gmail.com>
2020-05-27 15:58:12 +0100

    Round temperature values

--- src/components/SolCard.js
+++ src/components/SolCard.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 
 class SolCard extends Component {
   render () {
-    const sol = this.props.solNum
-    const avTemp = this.props.data.AT.av
-    const minTemp = this.props.data.AT.mn
-    const maxTemp = this.props.data.AT.mx
+    const sol = this.props.solNum;
+    const avTemp = Math.round(this.props.data.AT.av);
+    const mnTemp = Math.round(this.props.data.AT.mn);
+    const mxTemp = Math.round(this.props.data.AT.mx);
 
     return (
       <div>
-        Sol {sol}: {avTemp}
+        Sol {sol}: {avTemp}°C
       </div>
     )
   }
