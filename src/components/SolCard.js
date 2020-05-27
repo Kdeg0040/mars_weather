dcef2ec63872441e587e678e5df0ba235c9e3dd5
karlo <deguzmankarlo@gmail.com>
2020-05-27 17:44:29 +0100

    Display equivalent earth date

--- src/components/SolCard.js
+++ src/components/SolCard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import moment from 'moment';
 
 class SolCard extends Component {
   render () {
@@ -6,11 +7,14 @@ class SolCard extends Component {
     const avTemp = Math.round(this.props.data.AT.av);
     const mnTemp = Math.round(this.props.data.AT.mn);
     const mxTemp = Math.round(this.props.data.AT.mx);
+    const earthDate = moment(this.props.data.First_UTC).format('DD MMM');
 
     return (
       <div className="column">
         <div className="box">
           <h1 className="title is-4">Sol {sol} </h1>
+          <h1 className="subtitle is-6">{earthDate}</h1>
+          <p></p>
           <h1 className="title is-2 has-text-centered">{avTemp}°C</h1>
           <p>min: {mnTemp}°C</p>
           <p>max: {mxTemp}°C</p>
