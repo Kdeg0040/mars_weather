a5d85be7215e49585e46f942a1515171c848860b
karlo <deguzmankarlo@gmail.com>
2020-05-27 22:13:09 +0100

    Re-configure Bulma for full customization

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from './components/Weather';
 
 function App() {
   return (
-    <div className="hero is-fullheight ">
+    <div className="hero is-fullheight">
       <Weather />
     </div>
   );
