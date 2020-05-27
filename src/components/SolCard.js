84ef2243c9ac09adfaa4acea23f711572c679c3f
karlo <deguzmankarlo@gmail.com>
2020-05-27 15:41:35 +0100

    Save key data as constants for easier access

--- src/components/SolCard.js
+++ src/components/SolCard.js
@@ -2,9 +2,14 @@ import React, {Component} from 'react';
 
 class SolCard extends Component {
   render () {
+    const sol = this.props.solNum
+    const avTemp = this.props.data.AT.av
+    const minTemp = this.props.data.AT.mn
+    const maxTemp = this.props.data.AT.mx
+
     return (
       <div>
-        Sol {this.props.solNum}: {this.props.data.AT.av}
+        Sol {sol}: {avTemp}
       </div>
     )
   }
