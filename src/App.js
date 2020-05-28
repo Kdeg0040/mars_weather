4bbda2b32d23cc9c57858af2c3b01b07865a40ea
karlo <deguzmankarlo@gmail.com>
2020-05-28 14:13:34 +0100

    Center title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Weather from './components/Weather';
 function App() {
   return (
     <div className="hero is-fullheight has-background">
-      <div className="title is-size-1">Mars Weather</div>
+      <p className="title"></p>
+      <div className="title is-size-1 has-text-centered">Mars Weather</div>
       <img className="hero-background is-transparent" src="https://www.nasa.gov/sites/default/files/thumbnails/image/pia22228.jpg" alt=""></img>
       <Weather />
     </div>
